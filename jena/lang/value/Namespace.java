2c09eb1fa7fffaebe92313c4466f509deb882866
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-12-16 17:11:19 +0100

    int function

--- jena/lang/value/Namespace.java
+++ jena/lang/value/Namespace.java
@@ -14,6 +14,7 @@ public interface Namespace
     static final Namespace functions = new HashMapNamespace(action ->
     {
         action.call("box", () -> new FunctionValue("boxArg", BoxValue::new));
+        action.call("int", () -> new FunctionValue("toInt", arg -> new NumberValue(Single.of(arg).integer())));
     });
 
     static final Namespace standard = new StorageNamespace(
