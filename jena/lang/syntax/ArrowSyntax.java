dea14fe484a42086389d5db041dde11334d7fb63
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-12-28 18:41:59 +0100

    function self-reference support

--- jena/lang/syntax/ArrowSyntax.java
+++ jena/lang/syntax/ArrowSyntax.java
@@ -3,8 +3,8 @@ package jena.lang.syntax;
 import jena.lang.text.Text;
 import jena.lang.text.TextWriter;
 import jena.lang.value.FunctionValue;
+import jena.lang.value.HashMapNamespace;
 import jena.lang.value.Namespace;
-import jena.lang.value.SingleNamespace;
 import jena.lang.value.ValueFunction;
 
 public final class ArrowSyntax implements Syntax
@@ -21,10 +21,15 @@ public final class ArrowSyntax implements Syntax
     @Override
     public ValueFunction value(Namespace namespace)
     {
-        var argText = Text.of(argument);
-        return ValueFunction.of(new FunctionValue(argText.string(), arg ->
+        var argText = Text.of(argument).string();
+        ValueFunction[] self = { null };
+        return self[0] = ValueFunction.of(new FunctionValue(argText, arg ->
         {
-            return expression.value(namespace.nested(new SingleNamespace(argText, ValueFunction.of(arg)))).call();
+            return expression.value(namespace.nested(new HashMapNamespace(action ->
+            {
+                action.call(argText, ValueFunction.of(arg));
+                action.call("self", self[0]);
+            }))).call();
         }));
     }
 
